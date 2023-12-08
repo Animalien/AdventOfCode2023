e76435f2bee8d6956c9d34c7e1f81b9445df9935
Animalien <Animalien@users.noreply.github.com>
2023-12-08 12:34:38 -0800

    Day 1 Part 1

--- Problem1.cpp
+++ Problem1.cpp
@@ -6,16 +6,57 @@
 class Problem1 : public ProblemBase
 {
 public:
-	virtual int GetProblemNum() const override { return 1; }
+    virtual int GetProblemNum() const override { return 1; }
 
-	virtual void Run() override
-	{
-		printf("Test:\n\n");
+    virtual void Run() override
+    {
+        printf("Test:\n");
+        RunProblem("Day1Example.txt", true);
 
-		printf("\n\nInput:\n\n");
-	}
+        printf("\n\nInput:\n");
+        RunProblem("Day1Input.txt", false);
+    }
 
 private:
+    void RunProblem(const char* fileName, bool verbose)
+    {
+        StringList lines;
+        ReadFileLines(fileName, lines);
+
+        BigInt total = 0;
+        for (const std::string& line: lines)
+        {
+            BigInt firstDigit = 0;
+            for (auto iter = line.cbegin(); iter != line.cend(); ++iter)
+            {
+                const char ch = *iter;
+                if (std::isdigit(ch))
+                {
+                    firstDigit = (BigInt)(int)(ch - '0');
+                    break;
+                }
+            }
+
+            BigInt secondDigit = 0;
+            for (auto iter = line.crbegin(); iter != line.crend(); ++iter)
+            {
+                const char ch = *iter;
+                if (std::isdigit(ch))
+                {
+                    secondDigit = (BigInt)(int)(ch - '0');
+                    break;
+                }
+            }
+
+            BigInt number = firstDigit * 10 + secondDigit;
+            total += number;
+
+            if (verbose)
+                printf("  line = '%s', number = %lld, total = %lld\n", line.c_str(), number, total);
+        }
+
+        printf("Total = %lld\n", total);
+    }
 };
 
 Problem1 problem1;
