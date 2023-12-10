7bf9684559865d9446c1f14cd3e7ccf5f690b800
Animalien <Animalien@users.noreply.github.com>
2023-12-10 14:46:41 -0800

    Day 5, Part 2

--- Problem5.cpp
+++ Problem5.cpp
@@ -3,6 +3,9 @@
 
 #include "AdventOfCode2023.h"
 
+#define myassert(arg)
+//#define myassert(arg) assert(arg)
+
 class Problem5 : public ProblemBase
 {
 public:
@@ -22,8 +25,12 @@ private:
         BigInt end = -1;
         BigInt offset = 0;
 
+        Span() = default;
         Span(BigInt s) : start(s), num(-1), end(-1), offset(0) {}
-        Span(BigInt dest, BigInt src, BigInt n) : start(src), num(n), end(start + num - 1), offset(dest - src) {}
+        Span(BigInt dest, BigInt src, BigInt n) : start(src), num(n), end(start + num - 1), offset(dest - src)
+        {
+            myassert(start >= 0);
+        }
 
         operator BigInt() const { return start; }
     };
@@ -41,6 +48,7 @@ private:
 
         BigInt MapNumber(BigInt input) const
         {
+            myassert(input >= 0);
             BigInt spanIndex = -1;
             if (lastInput == (input - 1))
             {
@@ -50,8 +58,7 @@ private:
             }
             else
             {
-                const auto b = std::upper_bound(spanList.begin(), spanList.end(), input);
-                spanIndex = b - spanList.begin() - 1;
+                spanIndex = FindSpanIndexForInput(input);
             }
 
             lastInput = input;
@@ -59,6 +66,12 @@ private:
 
             const Span& span = spanList[spanIndex];
             return input + span.offset;
+        }
+
+        BigInt FindSpanIndexForInput(BigInt input) const
+        {
+            const auto b = std::upper_bound(spanList.begin(), spanList.end(), input);
+            return b - spanList.begin() - 1;
         }
     };
     typedef std::unordered_map<std::string, Map> MapMap;
@@ -90,7 +103,9 @@ private:
 
         // parse the maps
 
-        if (verbose)
+        const bool printMaps = true;        //verbose;
+
+        if (printMaps)
             printf("\n  Maps:\n");
 
         MapMap mapMap;
@@ -105,7 +120,7 @@ private:
             newMap.source = mapNameTokens[0];
             newMap.dest = mapNameTokens[2];
 
-            if (verbose)
+            if (printMaps)
                 printf("    Map %s to %s:\n", newMap.source.c_str(), newMap.dest.c_str());
 
             ++lineIndex;
@@ -131,7 +146,25 @@ private:
             Span postSpan(postSpanStart);
             newMap.spanList.insert(newMap.spanList.end(), postSpan);
 
-            if (verbose)
+            for (BigInt i = 1; i < (BigInt)newMap.spanList.size(); ++i)
+            {
+                const BigInt prevEnd = newMap.spanList[i - 1].end;
+                const BigInt requiredStart = prevEnd + 1;
+                const BigInt currStart = newMap.spanList[i].start;
+                if (currStart > requiredStart)
+                {
+                    // we have a gap, so insert a 0-offset span between
+                    Span newSpan;
+                    newSpan.start = requiredStart;
+                    newSpan.end = currStart - 1;
+                    newSpan.num = newSpan.end - newSpan.start + 1;
+                    newSpan.offset = 0;
+                    newMap.spanList.insert(newMap.spanList.begin() + i, newSpan);
+                    ++i;
+                }
+            }
+
+            if (printMaps)
             {
                 for (const Span& span: newMap.spanList)
                 {
@@ -148,51 +181,34 @@ private:
             ++lineIndex;
         }
 
-        // now run each of the seeds through the maps, and find the lowest result (Part 1)
+        // now MERGE all the maps
+
+        Map mergedMap = mapMap["seed"];
+        Map* mapToMerge = &(mapMap[mergedMap.dest]);
+        for (;;)
+        {
+            Map newMergedMap;
+            MergeMaps(mergedMap, *mapToMerge, newMergedMap);
+            mergedMap = newMergedMap;
+
+            if (mapToMerge->dest == "location")
+                break;
+
+            mapToMerge = &(mapMap[mapToMerge->dest]);
+        }
+
+        // now run each of the seeds through the merged map, and find the lowest result (Part 1)
 
         if (verbose)
-            printf("\n  Seeds through Maps, Part 1:\n");
-
-        Map*const seedMap = &(mapMap["seed"]);
-        Map* finalMap = nullptr;
+            printf("\n  Seeds through Merged Map, Part 1:\n");
 
         BigInt lowestLocation = -1;
         for (BigInt seed: seeds)
         {
+            const BigInt num = mergedMap.MapNumber(seed);
+
             if (verbose)
-                printf("    Seed %lld:\n", seed);
-
-            BigInt num = seed;
-            Map* map = seedMap;
-            for (;;)
-            {
-                if (verbose)
-                    printf("      Num %lld goes through map %s-to-%s, comes out ", num, map->source.c_str(), map->dest.c_str());
-
-                num = map->MapNumber(num);
-
-                if (verbose)
-                    printf("%lld\n", num);
-
-                if (finalMap)
-                {
-                    if (map == finalMap)
-                        break;
-                }
-                else
-                {
-                    if (map->dest == "location")
-                    {
-                        finalMap = map;
-                        break;
-                    }
-                }
-
-                if (!map->destPtr)
-                    map->destPtr = &(mapMap[map->dest]);
-
-                map = map->destPtr;
-            }
+                printf("      Seed %lld goes through merged map, comes out %lld\n", seed, num);
 
             if ((lowestLocation < 0) || (num < lowestLocation))
             {
@@ -210,10 +226,10 @@ private:
 
         printf("  Lowest location found = %lld\n", lowestLocation);
 
-        // now run each of the seeds through the maps, and find the lowest result (Part 1)
+        // now run each of the seeds through the merged map, and find the lowest result (Part 2)
 
         if (verbose)
-            printf("\n  Seeds through Maps, Part 2:\n");
+            printf("\n  Seeds through Merged Map, Part 2:\n");
 
         lowestLocation = -1;
         for (BigInt seedListIndex = 0; seedListIndex < (BigInt)seeds.size(); seedListIndex += 2)
@@ -226,41 +242,10 @@ private:
 
             for (BigInt seed = seed0; seed <= seed1; ++seed)
             {
+                const BigInt num = mergedMap.MapNumber(seed);
+
                 if (verbose)
-                    printf("      Seed %lld:\n", seed);
-
-                BigInt num = seed;
-                Map* map = seedMap;
-                for (;;)
-                {
-                    if (verbose)
-                        printf(
-                            "        Num %lld goes through map %s-to-%s, comes out ", num, map->source.c_str(), map->dest.c_str());
-
-                    num = map->MapNumber(num);
-
-                    if (verbose)
-                        printf("%lld\n", num);
-
-                    if (finalMap)
-                    {
-                        if (map == finalMap)
-                            break;
-                    }
-                    else
-                    {
-                        if (map->dest == "location")
-                        {
-                            finalMap = map;
-                            break;
-                        }
-                    }
-
-                    if (!map->destPtr)
-                        map->destPtr = &(mapMap[map->dest]);
-
-                    map = map->destPtr;
-                }
+                    printf("      Seed %lld goes through merged map, comes out %lld\n", seed, num);
 
                 if ((lowestLocation < 0) || (num < lowestLocation))
                 {
@@ -278,6 +263,69 @@ private:
         }
 
         printf("  Lowest location found = %lld\n", lowestLocation);
+    }
+
+    void MergeMaps(const Map& map1, const Map& map2, Map& newMap)
+    {
+        newMap = Map();
+
+        BigInt map1SpanIndex = 0;
+        BigInt span1Start = 0;
+
+        for (;;)
+        {
+            const BigInt span2Start = map1.MapNumber(span1Start);
+            myassert(span2Start >= 0);
+            const BigInt map2SpanIndex = map2.FindSpanIndexForInput(span2Start);
+
+            myassert((map1.spanList[map1SpanIndex].end < 0) || (map1.spanList[map1SpanIndex].end >= span1Start));
+            const BigInt span1Remaining =
+                (map1.spanList[map1SpanIndex].end >= 0) ? (map1.spanList[map1SpanIndex].end - span1Start + 1) : MAX_BIG_INT;
+
+            myassert((map2.spanList[map2SpanIndex].end < 0) || (map2.spanList[map2SpanIndex].end >= span2Start));
+            const BigInt span2Remaining =
+                (map2.spanList[map2SpanIndex].end >= 0) ? (map2.spanList[map2SpanIndex].end - span2Start + 1) : MAX_BIG_INT;
+
+            if (span1Remaining <= span2Remaining)
+            {
+                // last merged span bit from span 1
+
+                Span newSpan;
+                newSpan.start = span1Start;
+                if (span1Remaining < MAX_BIG_INT)
+                {
+                    newSpan.num = span1Remaining;
+                    newSpan.end = newSpan.start + newSpan.num - 1;
+                }
+                else
+                {
+                    newSpan.num = -1;
+                    newSpan.end = -1;
+                }
+                newSpan.offset = map1.spanList[map1SpanIndex].offset + map2.spanList[map2SpanIndex].offset;
+                newMap.spanList.push_back(newSpan);
+
+                ++map1SpanIndex;
+                if (map1SpanIndex >= (BigInt)map1.spanList.size())
+                    break;
+
+                span1Start = map1.spanList[map1SpanIndex].start;
+            }
+            else
+            {
+                // make a piece equal to this stretch from span 2
+
+                Span newSpan;
+                newSpan.start = span1Start;
+                newSpan.num = span2Remaining;
+                newSpan.end = newSpan.start + newSpan.num - 1;
+                newSpan.offset = map1.spanList[map1SpanIndex].offset + map2.spanList[map2SpanIndex].offset;
+                newMap.spanList.push_back(newSpan);
+
+                myassert(newSpan.num >= 0);
+                span1Start += newSpan.num;
+            }
+        }
     }
 };
 
